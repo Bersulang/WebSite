888950b1fbcbf2fabe6fea7c9748658f97c9ec00
LazyPig <152492591+Bersulang@users.noreply.github.com>
2025-05-06 22:14:32 +0800

    update: add action.js to html-study

---
+++ html-study/action.js
@@ -0,0 +1,12 @@
+console.log("life is perfect");
+var man = {
+    name : "蔡徐坤",
+    age : 25,
+    gender : "男",
+    like : function(){
+        console.log("我喜欢唱跳rap篮球");
+    }
+};
+function myFunction(){
+    document.getElementById("demo").innerHTML = Date();
+}
