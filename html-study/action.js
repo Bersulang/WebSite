9d18d4d849582905d167eb89b17810a17dbd2cb4
LazyPig <152492591+Bersulang@users.noreply.github.com>
2025-05-07 23:56:37 +0800

    update action.js and rename test to test.html

--- html-study/action.js
+++ html-study/action.js
@@ -6,7 +6,7 @@ var man = {
     like : function(){
         console.log("我喜欢唱跳rap篮球");
     }
-};
+}; //除var外还有let和const(定义常量)
 function myFunction(){
-    document.getElementById("demo").innerHTML = Date();
+    document.getElementById("demo").innerHTML = Date(); //插入写法在html渲染完成后执行会覆盖掉原有的内容
 }
